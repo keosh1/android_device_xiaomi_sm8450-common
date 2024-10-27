b6f1544179c81f65234404479139c3b034e99482
Keosh <106485828+keosh1@users.noreply.github.com>
2024-10-27 11:30:18 +0300

    Temp Fix : Shim WFD with libgui_shim/libinput_shim
    need to check if it will work without any issues or not !!

--- extract-files.py
+++ extract-files.py
@@ -68,6 +68,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed(
