4d0038d97e96250f398d95e90cdb26d7d65f0ffe
keosh <conquer2152016@gmail.com>
2025-03-21 23:33:21 +0000

    Revert "sm8450-common: Add feature flag for IPSEC_TUNNEL_MIGRATION"
    This reverts commit dea2590340577f1cccd4a2b247d34ce52f52afbe.

--- common.mk
+++ common.mk
@@ -227,7 +227,6 @@ PRODUCT_PACKAGES += \
 
 # Network
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # NFC / Secure Element
